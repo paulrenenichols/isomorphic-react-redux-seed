bedfd1c714a9dab91da5c6e58a7031ac203a61de
Jason Burks <jason@howdy.design>
2016-08-29 13:35:54 -0500

    initial-design-002
    * styled tiles in body

--- client/containers/App.js
+++ client/containers/App.js
@@ -28,9 +28,17 @@ class App extends Component {
       <div className={'appContainer'}>
         <div className={'content hero'}>
         </div>
-        <div>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+        <section className={'tiles'}>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-3.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-4.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-5.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-1.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-2.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-3.jpg" alt /></div>
+          <div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/jeremiah-wilson-4.jpg" alt /></div>
+        </section>
         <div className={'socialBar'}>
           <h3>Social Footer</h3>
           <br />
@@ -38,7 +46,7 @@ class App extends Component {
             <a href="https://twitter.com/"><img title="Twitter" className={'socialIcon'} src="https://cdn4.iconfinder.com/data/icons/miu-flat-social/60/twitter-48.png" alt="Twitter" /></a>
             <a href="https://www.facebook.com/"><img title="Facebook" className={'socialIcon'} src="https://cdn4.iconfinder.com/data/icons/miu-flat-social/60/facebook-48.png" alt="Facebook" /></a>
             <a href="mailto: jason@howdy.design"><img title="Email" className={'socialIcon'} src="https://cdn4.iconfinder.com/data/icons/miu-flat-social/60/mail-48.png" alt="Email" /></a>
-            <a href="www.instagram.com/"><img title="Instagram" className={'socialIcon'} src="https://cdn3.iconfinder.com/data/icons/social-circle/512/social_6-48.png" alt="Instagram" /></a>
+            <a href="https://www.instagram.com/"><img title="Instagram" className={'socialIcon'} src="https://cdn3.iconfinder.com/data/icons/social-circle/512/social_6-48.png" alt="Instagram" /></a>
           </div>
         </div>
       </div>
